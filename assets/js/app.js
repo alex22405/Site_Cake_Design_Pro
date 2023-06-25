f29cd6707dea597806ded17cde05c61a1e114b19
avigon <vigonalexandre@hotmail.com>
2023-06-25 04:00:59 +0200

    changement d'images

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,14 +8,14 @@ window.addEventListener('scroll', function()
 )
 /*********************************************************************************************/
 // Apparition titre et p
-const titreSpans = document.querySelectorAll('.banner_content span');
+const titreSpans = document.querySelectorAll('.accueil_banner_content span');
 
 
 window.addEventListener('load', () => {
     const TL = gsap.timeline({paused:  true});
     
     TL
-    .staggerFrom(titreSpans, 1, {opacity: 0.2, ease: "power2.out"}, 0.4)
+    .staggerFrom(titreSpans, 4, {opacity: 0.2, ease: "power2.out"}, 0.4)
     TL.play()
 }
 ) 
@@ -31,6 +31,28 @@ function toggleMenu(){
     productMenu.style.display = (productMenu.style.display === "block") ? "none" : "block";
 }
 /*********************************************************************************************/
+// Apparition au scroll
+// Sélectionnez tous les éléments à animer
+const animatedElements = document.querySelectorAll('.animated-element');
+
+// Fonction de gestion de l'événement de défilement
+function handleScroll() {
+  animatedElements.forEach((element) => {
+    // Vérifiez si l'élément est visible à l'écran
+    const elementTop = element.getBoundingClientRect().top;
+    const elementBottom = element.getBoundingClientRect().bottom;
+
+    if (elementTop < window.innerHeight && elementBottom >= 0) {
+      // Ajoutez la classe 'is-visible' lorsque l'élément est visible
+      element.classList.add('is-visible');
+    }
+  });
+}
+
+// Écoutez l'événement de défilement et appelez la fonction de gestion correspondante
+window.addEventListener('scroll', handleScroll);
+// **********************************************************************************************
+
 // Apparition des fleurs
 // const imgs = document.querySelectorAll('img');
 // const TL = gsap.timeline({paused: true});
